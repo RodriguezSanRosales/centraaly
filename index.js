0d54c621042a1b5edfee986d22da7270f290921e
RodriguezSanRosales <san.rodzrosales@gmail.com>
2019-03-14 20:21:21 -0600

    se agrego nodemon

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const express = requiere('express');
+const express = require('express');
+const app = express();//ejecutamos express
+const port = 8080;
+const chalk = require('chalk');//pinta colores en la consola
+
+//rutas
+app.get('/',(req, res) => {
+    res.send('respondiendo c:');
+});
+// app.post();
+// app.post();
+// app.deleted();
+
+//Escuchar peticiones en un puerto
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+    console.log(chalk.magenta(`http://localhost:${port}`));
+
+});
